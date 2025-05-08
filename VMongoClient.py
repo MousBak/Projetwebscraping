0ba2b96ade06220a8f0965c5ec4d14ae88ae74d2
Bakayoko <159626506+MousBak@users.noreply.github.com>
2025-05-08 17:33:05 +0200

    Mise à jour du projet : structure, requirements.txt, etc.

--- VMongoClient.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["films_db"]
-print(db.list_collection_names())
-print(db.top_250_imdb.count_documents({}), "films dans la collection")
--- db_mongo.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-def get_mongo_connection():
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["films_db"]
-    return db
